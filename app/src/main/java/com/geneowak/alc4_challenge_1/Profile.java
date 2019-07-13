f3958c4b6045d6061a0d7ac9af9b2d5664500b84
geneowak <teugeneowak@gmail.com>
2019-07-14 01:24:29 +0300

    - Add profile pic. - Modify profile to meet described standards.

--- app/src/main/java/com/geneowak/alc4_challenge_1/Profile.java
+++ app/src/main/java/com/geneowak/alc4_challenge_1/Profile.java
@@ -1,7 +1,7 @@
 package com.geneowak.alc4_challenge_1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,4 +10,6 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+
 }
